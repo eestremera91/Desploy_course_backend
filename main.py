7d566978e95d56412d2edba3a6b8a6fc3fad94c5
eestremera91 <eestremera91@gmail.com>
2023-11-13 12:40:31 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from routers import movies, user
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.security import HTTPBearer
+from middlewares.error_handle import ErrorHandler
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app -- reload")
+    
+
+app = FastAPI()  #instanciamos FastApi
+app.title = "aplicación con FastAPI"
+app.version = "1.0"
+app.add_middleware(ErrorHandler)
+
+#routers: esto es para enlazar otras APIs a estas como API principal
+@app.get('/', tags=['home'])
+async def message():
+    return HTMLResponse('<h1> Hello World</h1')
+
+app.include_router(user.router)
+app.include_router(movies.router)
